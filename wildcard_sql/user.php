23bf7be83251f15d7490bc40982c3baea2d16521
Allta <francois.delrue@ynov.com>
2022-03-17 17:07:43 +0100

    sources

--- wildcard_sql/user.php
+++ wildcard_sql/user.php
@@ -1,112 +1,37 @@
-<link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type='text/css'>
-<style>
-body{
-  font-family: 'Open Sans', sans-serif;
-  background:#3498db;
-  margin: 0 auto 0 auto;  
-  width:100%; 
-  text-align:center;
-  margin: 20px 0px 20px 0px;   
-}
+<?php
+    // code source de user.php
+    $host = "localhost";
+    $user_mysql = "root";     // nom d'utilisateur de l'utilisateur de MySQL 
+    $password_mysql = "";     // mot de passe de l'utilisateur de MySQL
+    $database = "db";
 
-p{
-  font-size:12px;
-  text-decoration: none;
-  color:#ffffff;
-}
+    $db = mysqli_connect($host, $user_mysql, $password_mysql, $database);
+    mysqli_set_charset($db, "utf8");
+?>
 
-h1{
-  font-size:1.5em;
-  color:#525252;
-}
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title></title>
+        <meta charset="UTF-8" />
+    </head>
+    <body>
+        <?php
+            if(!empty($_POST['username']))
+            {
+                $user = mysqli_real_escape_string($db, $_POST['username']);
+                $query = "SELECT username FROM users WHERE password LIKE '".$_POST['password']."';";
+                $result = mysqli_query($db, $query);
 
-.box{
-  background:white;
-  width:300px;
-  border-radius:6px;
-  margin: 0 auto 0 auto;
-  padding:0px 0px 70px 0px;
-  border: #2980b9 4px solid; 
-}
-
-.email{
-  background:#ecf0f1;
-  border: #ccc 1px solid;
-  border-bottom: #ccc 2px solid;
-  padding: 8px;
-  width:250px;
-  color:#AAAAAA;
-  margin-top:10px;
-  font-size:1em;
-  border-radius:4px;
-}
-
-.password{
-  border-radius:4px;
-  background:#ecf0f1;
-  border: #ccc 1px solid;
-  padding: 8px;
-  width:250px;
-  font-size:1em;
-}
-
-.btn{
-  background:#2ecc71;
-  width:125px;
-  padding-top:5px;
-  padding-bottom:5px;
-  color:white;
-  border-radius:4px;
-  border: #27ae60 1px solid;
-  
-  margin-top:20px;
-  margin-bottom:20px;
-  float:left;
-  margin-left:16px;
-  font-weight:800;
-  font-size:0.8em;
-}
-
-.btn:hover{
-  background:#2CC06B; 
-}
-
-#btn2{
-  float:left;
-  background:#3498db;
-  width:125px;  padding-top:5px;
-  padding-bottom:5px;
-  color:white;
-  border-radius:4px;
-  border: #2980b9 1px solid;
-  
-  margin-top:20px;
-  margin-bottom:20px;
-  margin-left:10px;
-  font-weight:800;
-  font-size:0.8em;
-}
-
-#btn2:hover{ 
-background:#3594D2; 
-}
-
-</style>
-
-<form method="post" action="user.php">
-<div class="box">
-<h1>Dashboard</h1>
-
-<input type="text" name="username" value="username" class="email" />
-  
-<input type="password" name="password" value="password" class="email" />
-  
-<input type="submit" class="btn" value="Log In">
-  
-</div> <!-- End Box -->
-  
-</form>
-
-<p>Forgot your password? <u style="color:#f1c40f;">Click Here!</u></p>
-  
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
+                if(mysqli_num_rows($result) == 1)
+                {
+                    echo "<p>Utilisateur existant.</p>";
+                }
+                else
+                {
+                    echo "<p>Utilisateur inexistant.</p>";
+                }
+            }
+        ?>
+    </body>
+</html>
